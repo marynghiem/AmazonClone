167abd3dd84a50618632c0a39fac7d20df38aabd
marynghiem <marynghiem46@gmail.com>
2022-11-07 12:45:41 -0800

    added firebase into amazon clone project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAIH_93SEXAtK12d4fNR5crRwVC91t1DZI",
+  authDomain: "clone-3ade4.firebaseapp.com",
+  projectId: "clone-3ade4",
+  storageBucket: "clone-3ade4.appspot.com",
+  messagingSenderId: "729993136778",
+  appId: "1:729993136778:web:4d6fcc7826743be4dc3b73",
+  measurementId: "G-XSJVTPF2CT",
+};
