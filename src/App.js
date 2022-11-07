e34fb9053b3fee2b138f3037564f44ed47da11d9
marynghiem <marynghiem46@gmail.com>
2022-11-07 15:49:40 -0800

    created home and product component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      {/* Header */}
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
