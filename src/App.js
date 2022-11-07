ba47a44b603c7de2b6324f55dbbe977310f52588
marynghiem <marynghiem46@gmail.com>
2022-11-07 14:18:47 -0800

    downloaded material ui to use the icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
       {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Header = () => {
   return (
@@ -7,9 +8,22 @@ const Header = () => {
       <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
       <div className="header_search">
         <input className="header_search_input" type="text" />
-        {/* Logo */}
+        <SearchIcon className="header_search_icon" />
       </div>
-      <div className="header_nav"></div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_option_line_one">Hello Guest</span>
+          <span className="header_option_line_two">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_option_line_one">Returns</span>
+          <span className="header_option_line_two">Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_option_line_one">Your</span>
+          <span className="header_option_line_two">Prime</span>
+        </div>
+      </div>
     </div>
   );
 };
