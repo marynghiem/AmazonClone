6e60a7e6ae0de52a77b26604d732936d5de6b6c3
marynghiem <marynghiem46@gmail.com>
2022-11-08 14:04:21 -0800

    added react router dom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
