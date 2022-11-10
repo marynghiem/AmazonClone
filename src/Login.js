05f8c92ca7ed78fd65d5f250caa82967556e21dd
marynghiem <marynghiem46@gmail.com>
2022-11-10 14:44:36 -0800

    added the content of the login component

--- src/Login.js
+++ src/Login.js
@@ -11,6 +11,23 @@ const Login = () => {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
         />
       </Link>
+      <div className="login_container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>Email</h5>
+          <input type="text" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button type="submit" className="login_signin_button">
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our
+          Cookies Notice and our Interest-Based Ads Notice.
+        </p>
+        <button className="login_register_button">Create your Amazon Account</button>
+      </div>
     </div>
   );
 };
