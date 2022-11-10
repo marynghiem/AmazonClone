9c173b023c2aa1a4a9f9197d74a919be6a676976
marynghiem <marynghiem46@gmail.com>
2022-11-10 15:20:45 -0800

    added onclicks to login and state

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+    //some fancy firebase
+  };
+  const register = (e) => {
+    e.preventDefault();
+    //some fancy firebase stuff
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -15,10 +26,10 @@ const Login = () => {
         <h1>Sign-in</h1>
         <form>
           <h5>Email</h5>
-          <input type="text" />
+          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
           <h5>Password</h5>
-          <input type="password" />
-          <button type="submit" className="login_signin_button">
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit" className="login_signin_button" onClick={signIn}>
             Sign In
           </button>
         </form>
@@ -26,7 +37,9 @@ const Login = () => {
           By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our
           Cookies Notice and our Interest-Based Ads Notice.
         </p>
-        <button className="login_register_button">Create your Amazon Account</button>
+        <button onClick={register} className="login_register_button">
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
