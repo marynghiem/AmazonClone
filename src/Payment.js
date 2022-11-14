524886d8c2f428335d471401d749c51d5307bd82
marynghiem <marynghiem46@gmail.com>
2022-11-14 15:02:46 -0800

    created the route and component for payment

---
+++ src/Payment.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Payment.css";
+
+const Payment = () => {
+  return (
+    <div className="payment">
+      <div className="payment_container"></div>
+    </div>
+  );
+};
+
+export default Payment;
