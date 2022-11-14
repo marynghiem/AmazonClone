2f8e0a77fcad6d596673219dfe95e45cff3be153
marynghiem <marynghiem46@gmail.com>
2022-11-14 15:20:29 -0800

    finished first payment selection content

--- src/Payment.js
+++ src/Payment.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Payment.css";
+import { useStateValue } from "./StateProvider";
 
 const Payment = () => {
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
-      <div className="payment_container"></div>
+      <div className="payment_container">
+        {/*Payment Section - delivery address */}
+        <div className="payment_section">
+          <div className="payment_title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment_address">
+            <p>{user?.email}</p>
+          </div>
+        </div>
+        {/*Payment Section - review item*/}
+        <div className="payment_section"></div>
+        {/*Payment Section - payment method */}
+        <div className="payment_section"></div>
+      </div>
     </div>
   );
 };
