00875792b8de242857272c20f7f4d319e706972d
marynghiem <marynghiem46@gmail.com>
2022-11-15 14:46:31 -0800

    styled payment component and added state for payment component

--- src/Payment.js
+++ src/Payment.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CheckoutProduct from "./CheckoutProduct";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
 
 const Payment = () => {
   const [{ basket, user }, dispatch] = useStateValue();
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
   const stripe = useStripe();
   const elements = useElements();
 
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    //Listen for changes in the CardElement
+    //and display any errors as the customer types their card details
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
   return (
     <div className="payment">
       <div className="payment_container">
@@ -52,8 +66,21 @@ const Payment = () => {
             <h3>Payment Method</h3>
           </div>
           <div className="payment_details">
-            <form>
-              <CardElement />
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment_price_container">
+                <CurrencyFormat
+                  renderText={(value) => <h3> Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
             </form>
           </div>
         </div>
