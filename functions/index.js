63dcfb469b753692ec6deea3e99d4dd029b7129a
marynghiem <marynghiem46@gmail.com>
2022-11-17 14:16:57 -0800

    installed cors and stripe for the backend and made post and get request

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,33 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51M4Wn0BkE5gNImF1Cmdo99YM3QS7tjNwgJLuA2J1YmfJuyzXuCsVsyLKbjW0SntGHFvIZIAzZhtYPUTtWWtnXKoZ002vqLEtLn"
+);
+//API
+
+//App config
+const app = express();
+
+//Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+//API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  console.log("Payment Request Recieved BOOM !!! for this amount >>>", total);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, //subunits of the currency
+    currency: "usd",
+  });
+  //OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.clientSecret,
+  });
+});
+
+//Listen command
+exports.api = functions.https.onRequest(app);
