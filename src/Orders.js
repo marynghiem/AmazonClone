1e017cad3897869bc39c7ea6bfca7f9d43310fa8
marynghiem <marynghiem46@gmail.com>
2022-11-23 16:53:51 -0800

    get the data from firestore and save it within state when the page first loads

--- src/Orders.js
+++ src/Orders.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { db } from "./firebase";
 import "./Orders.css";
+import { useStateValue } from "./StateProvider";
 
 const Orders = () => {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    db.collection("users")
+      .doc(user?.uid)
+      .collection("orders")
+      .orderBy("created", "desc")
+      .onSnapshot((snapshot) =>
+        setOrders(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
   return (
     <div className="orders">
-      <h1>Your orders</h1>
+      <h1>Your Orders</h1>
     </div>
   );
 };
