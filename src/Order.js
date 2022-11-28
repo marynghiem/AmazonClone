d09ebd4725021657a92b262c6ddf734f769996eb
marynghiem <marynghiem46@gmail.com>
2022-11-28 14:19:59 -0800

    created single order component

---
+++ src/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Order.css";
+
+const Order = ({ order }) => {
+  return (
+    <div className="order">
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Order;
--- src/Orders.js
+++ src/Orders.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { db } from "./firebase";
+import Order from "./Order";
 import "./Orders.css";
 import { useStateValue } from "./StateProvider";
 
@@ -29,6 +30,11 @@ const Orders = () => {
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+      <div className="orders_order">
+        {orders?.map((order) => (
+          <Order order={order} />
+        ))}
+      </div>
     </div>
   );
 };
